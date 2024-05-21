e58dcac0ac94ee2d3da1b5fec1dcb4fa48523eea
santozzz <davd.c9ol@gmail.com>
2024-05-20 21:14:38 -0300

    criação da navabar

--- frontend/src/components/ALL/Navbar.vue
+++ frontend/src/components/ALL/Navbar.vue
@@ -20,7 +20,72 @@
             </nav>
         </header>
         <nav class="navbar">
-
+          <ul class="nav-list">
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-home"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/">Página inicial</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-briefcase"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/vagaemprego">Vagas de Emprego</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-bell"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/index.vue">Notificações</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-book-open"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/index.vue">Seu Currículo</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-bookmark"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/index.vue">Favoritos</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-laptop"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/index.vue">Cursos</router-link>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fa-solid fa-headset"></i>
+              </div>
+              <div class="namePage">
+                <router-link to="/index.vue">Entrevista</router-link>
+              </div>
+            </li>
+          </ul>
+          <div class="container-contact">
+            <div class="line"></div>
+            <div class="contacts">
+              <i class="fa-brands fa-whatsapp"></i>
+              <i class="fa-brands fa-facebook"></i>
+              <i class="fa-brands fa-instagram"></i>
+            </div>
+          </div>
         </nav>
     </div>
 </template>
@@ -50,6 +115,9 @@ export default {
       position: fixed;
       height: 90vh;
       width: 18%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
       background-color: #131921;
     }
     header{
@@ -132,8 +200,74 @@ export default {
       display: flex;
       flex-direction: column;
     }
-    /* Animação para o ícone de seta */
-    .fa-angle-up {
-        transform: rotate(180deg);
+    .nav-list{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      height: 55vh;
+      list-style: none;
+      margin-top: 10%;
+    }
+    .nav-list li{
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      width: 100%;
+      border-radius: 10px;
+      transition: .5s ease;
+    }
+    .nav-list li:hover{
+      background-color: #252c35;
+      cursor: pointer;
+    }
+    .nav-list li .icon{
+      width: 10%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .nav-list li i{
+      color: #fff;
+    }
+    .nav-list li .namePage{
+      width: 80%;
+      display: flex;
+      justify-content: start;
+      align-items: center;
+    }
+    .nav-list li a{
+      color: #fff;
+      font-size: 14px;
+      font-weight: 500;
+      letter-spacing: 1px;
+      text-decoration: none;
+    }
+    .line{
+      height: 1px;
+      width: 80%;
+      background-color: #fff;
+    }
+    .container-contact{
+      width: 90%;
+      height: 30%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      flex-direction: column;
+    }
+    .contacts{
+      display: flex;
+      width: 55%;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .contacts i{
+      color: #fff;
+      font-size: 15px;
+      border: 1px solid #fff;
+      border-radius: 50%;
+      padding: 8px;
+      cursor: pointer;
     }
 </style>
