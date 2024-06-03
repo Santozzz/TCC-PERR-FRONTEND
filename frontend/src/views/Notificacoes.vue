0c9e5cf62d988445d11ff0b56a6b2bc04ecb0f04
santozzz <davd.c9ol@gmail.com>
2024-06-03 20:22:48 -0300

    importação do componente gridNotify para notificacoes

--- frontend/src/views/Notificacoes.vue
+++ frontend/src/views/Notificacoes.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <Navbar />
+        <gridNotify />
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/ALL/Navbar.vue';
+import gridNotify from '@/components/Notificacoes/gridNotify.vue';
 
 export default {
     name: 'Notificacoes',
     components: {
         Navbar,
+        gridNotify,
     }
 }
 </script>
