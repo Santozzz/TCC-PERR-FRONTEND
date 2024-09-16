b166cd2fb9a06a9cf2720736623809f8eb02b63e
santozzz <davd.c9ol@gmail.com>
2024-09-16 19:07:42 -0300

    update

---
+++ frontend/src/components/Colaboradores/colaboradores.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    margin: 30px 20%;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+h3 {
+    font-size: 20px;
+    color: #252F3F;
+    width: 95%;
+}
+</style>
--- frontend/src/views/Colaboradores.vue
+++ frontend/src/views/Colaboradores.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <Navbar />
+        <Colab />
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/ALL/Navbar.vue';
+import Colab from '@/components/Colaboradores/colaboradores.vue';
 
 export default {
     name: 'Cursos',
     components: {
         Navbar,
+        Colab,
     }
 }
 </script>
