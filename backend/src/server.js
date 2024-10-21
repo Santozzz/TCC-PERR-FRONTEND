7fef77cd90224195566db13099d63c42ddf12872
David Da Silva Santos <107252394+Santozzz@users.noreply.github.com>
2024-10-21 14:29:53 -0300

    Delete backend directory

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from './app.js'
-
-const PORT = process.env.PORT || 3000
-
-// Escutar a porta
-app.listen(PORT, () => {
-    console.log(`O servidor esta rodando no endereço: http://localhost:${PORT}`);
-})      
