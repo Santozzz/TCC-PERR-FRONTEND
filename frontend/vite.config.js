718e0e52aa3f7320a9777a3e23ad92053ea147d2
DaniloKeithi <danilomakiya@gmail.com>
2024-11-17 17:48:49 -0300

    dasdadad

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +13,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  base: '/', // Base do projeto (necessária para roteamento)
+  build: {
+    outDir: 'dist', // Define a pasta de saída (padrão é 'dist')
+  },
+});
