fb99752f92e62ff40176f48fbdf20a497dbae09b
DaniloKeithi <danilomakiya@gmail.com>
2024-11-17 19:31:36 -0300

    dasdada

--- frontend/src/components/Home/grid.vue
+++ frontend/src/components/Home/grid.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="header">
                 <div class="user-notify">
-                    <p>[Usuario]</p>
+                    <p>[UsuarioOOOOOOOO]</p>
                     <!-- <i class="fa-solid fa-bell" :class="{ 'notifyOpen': isBellNotify }" @click="toggleNotify"></i>
                     <div class="notifications" :class="['notify', { 'notifyOpen': isBellNotify }]">
                         <h4>Notificações</h4>
