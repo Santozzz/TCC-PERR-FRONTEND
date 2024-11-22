ec2b17dec1750fafea898974acff83fc36d51d46
DaniloKeithi <danilomakiya@gmail.com>
2024-11-22 20:01:41 -0300

    home das empresas

--- frontend/src/views/homedaempresa.vue
+++ frontend/src/views/homedaempresa.vue
@@ -1,22 +1,21 @@
+<template>
+    <div>
+        <containerPrincipal />
+    </div>
+</template>
+
+
 <script>
-import headerCadastro from '../components/HomeEmpresas/.vue'
 import containerPrincipal from '../components/HomeEmpresas/homeempresas.vue'
 
 export default {
     name: 'Cadastrodaempresa',
     components: {
-        headerCadastro,
         containerPrincipal,
     }, 
 }
 </script>
 
-<template>
-    <div>
-        <headerCadastro />
-        <containerPrincipal />
-    </div>
-</template>
 
 <style scoped>
 
